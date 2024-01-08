0b73e8fa0cbad72fd394fa7cab17c4be76690138
Sathish Babu <sbabu@progress.com>
2024-01-08 16:23:12 +0530

    update version
    Signed-off-by: Sathish Babu <sbabu@progress.com>

--- lib/train/k8s/container.rb
+++ lib/train/k8s/container.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "container/version"
+require_relative "container/platform"
+require_relative "container/connection"
+require_relative "container/transport"
 
 module Train
   module K8s
