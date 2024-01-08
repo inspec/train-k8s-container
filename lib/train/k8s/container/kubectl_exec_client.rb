d657e7bb5c7e9b43331d4f899f87bc3342402163
Sathish Babu <sbabu@progress.com>
2024-01-08 17:27:14 +0530

    kubectl exec client to execute commands on the container
    Signed-off-by: Sathish Babu <sbabu@progress.com>

---
+++ lib/train/k8s/container/kubectl_exec_client.rb
@@ -0,0 +1,53 @@
+require "mixlib/shellout" unless defined?(Mixlib::ShellOut)
+require "train/options" # only to load the following requirement `train/extras`
+require "train/extras"
+
+module Train
+  module K8s
+    module Container
+      class KubectlExecClient
+        attr_reader :pod, :container_name, :namespace
+
+        DEFAULT_NAMESPACE = "default".freeze
+
+        def initialize(pod:, namespace: nil, container_name: nil)
+          @pod = pod
+          @container_name = container_name
+          @namespace = namespace || DEFAULT_NAMESPACE
+        end
+
+        def execute(command)
+          instruction = build_instruction(command)
+          shell = Mixlib::ShellOut.new(instruction)
+          res = shell.run_command
+          Train::Extras::CommandResult.new(res.stdout, res.stderr, res.exitstatus)
+        rescue Errno::ENOENT => _e
+          Train::Extras::CommandResult.new("", "", 1)
+        end
+
+        private
+
+        def shell
+          @shell ||= Mixlib::ShellOut.new(instruction)
+        end
+
+        def build_instruction(command)
+          ["kubectl exec"].tap do |arr|
+            arr << "--stdin"
+            arr << pod if pod
+            if namespace
+              arr << "-n"
+              arr << namespace
+            end
+            if container_name
+              arr << "-c"
+              arr << container_name
+            end
+            arr << "--"
+            arr << command
+          end.join("\s")
+        end
+      end
+    end
+  end
+end
