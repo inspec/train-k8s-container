47188d240d2f8c2a80715508e86b4e527cf3196f
Sathish Babu <sbabu@progress.com>
2024-01-09 19:55:04 +0530

    remove redundant spec
    Signed-off-by: Sathish Babu <sbabu@progress.com>

--- spec/train/k8s/container_spec.rb
+++ spec/train/k8s/container_spec.rb
@@ -5,8 +5,4 @@ RSpec.describe Train::K8s::Container do
   it "has a version number" do
     expect(Train::K8s::Container::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
